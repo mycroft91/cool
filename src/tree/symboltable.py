f9b452155ce1c7c01529a6cfd41d61b3751cd76c
Madhukar yerraguntla <madhukar.yerraguntla@gmail.com>
2018-09-06 08:52:57 +0530

    why can't I work on this?

---
+++ src/tree/symboltable.py
@@ -0,0 +1,7 @@
+
+
+class SymbolTable():
+    def __init__(self):
+        pass
+    
+    
---
+++ src/tree/typechecker.py
@@ -0,0 +1,8 @@
+import sys
+import os
+from tree import *
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+class TypeChecker():
+    pass
+
